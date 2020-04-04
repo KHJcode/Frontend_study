dd6da5b78b3b82c99891d7250c13b7189262e09c
KHJcode <kimstudswcode4485@gmail.com>
2020-04-04 17:21:52 +0900

    200404

--- 객체 지향/class.js
+++ 객체 지향/class.js
@@ -1,13 +1,25 @@
 class Person {
+    constructor(name, first, second, third){
+        this.name = name;
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    sum(){
+        return 'prototype: '+(this.first + this.second + this.third);
+    }
+}
 
+class PersonPlus extends Person { // extends = 확장 ( Person 의 요소들을 상속 받는다. )
+    avg(){
+        return (this.first + this.second)/2;
+    }
 }
 
 
-var kim = new Person();
+var kim = new PersonPlus('kim', 10, 20, 30);
 
-console.log('kim', kim);
-
-
-
+console.log('kim.sum() : ', kim.sum());
+console.log('kim.avg() : ', kim.avg());
 
 
